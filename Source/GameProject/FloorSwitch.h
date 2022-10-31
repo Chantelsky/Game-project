513fc13e1bf878de9ecb817bec7fe802489a4b05
Chantel Punt <csky.punt@gmail.com>
2022-10-31 16:19:38 +1000

    Floor switch continued

--- Source/GameProject/FloorSwitch.h
+++ Source/GameProject/FloorSwitch.h
@@ -27,6 +27,14 @@ public:
 	/** Door to move when the floor switch is stepped on **/
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Floor Switch")
 	UStaticMeshComponent*Door;
+
+	/** Initial Location for the door **/
+	UPROPERTY(BlueprintReadWrite, Category= "Floor Switch")
+	FVector InitialDoorLocation;
+
+	/** Initial Location for the floor switch **/
+	UPROPERTY(BlueprintReadWrite, Category= "Floor Switch")
+	FVector InitialSwitchLocation;
 	
 protected:
 	// Called when the game starts or when spawned
@@ -41,4 +49,23 @@ public:
 
 	UFUNCTION()
 	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Floor Switch")
+	void RaiseDoor();
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Floor Switch")
+	void LowerDoor();
+	
+	UFUNCTION(BlueprintImplementableEvent, Category="Floor Switch")
+	void RaiseFloorSwitch();
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Floor Switch")
+	void LowerFloorSwitch();
+
+	UFUNCTION(BlueprintCallable, Category="Floor Switch")
+	void UpdateDoorLocation(float Z);
+
+	UFUNCTION(BlueprintCallable, Category="Floor Switch")
+	void UpdateFloorSwitchLocation(float Z);
+	
 };
